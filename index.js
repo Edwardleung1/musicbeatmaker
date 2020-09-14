f258a0622ed0b8d60dbd6c438bebe7b6bd1cb93e
Edward Leung <edward_leung_12@hotmail.com>
2020-09-14 21:01:19 +0100

    added the sound on each pads.

--- index.js
+++ index.js
@@ -5,4 +5,16 @@ window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   // 2. get all small div inside .pads
   const pads = document.querySelectorAll(".pads div");
+
+  // Let's get going with the sound here
+  // loop over each pads, with (index) for each sound
+  pads.forEach((pad, index) => {
+    // addEventListener to each pad
+    pad.addEventListener("click", function () {
+      // reset current track time
+      sounds[index].currentTime = 0;
+      // select each sound and play
+      sounds[index].play();
+    });
+  });
 });
