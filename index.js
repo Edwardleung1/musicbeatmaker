32e11d9232c99f82be650753141e1d5c82e0aa3f
Edward Leung <edward_leung_12@hotmail.com>
2020-09-14 20:52:13 +0100

    added element selectors and window content load function

---
+++ index.js
@@ -0,0 +1,8 @@
+// after the window content is loaded, run this function
+window.addEventListener("load", () => {
+  // element selectors
+  // 1. get all the audio sounds
+  const sounds = document.querySelectorAll(".sound");
+  // 2. get all small div inside .pads
+  const pads = document.querySelectorAll(".pads div");
+});
